b3b1eaf4b86a785e3c3487629a941be68408a76f
DucChanHUST <kuntakufcvn@gmail.com>
2024-11-10 14:45:19 +0700

    eidt size

--- plot_pareto_front.py
+++ plot_pareto_front.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 dataset = "100_1"
-run = 6
+run = 1
 filename = f"./result/f/moead/{dataset}/{run}.csv"
 df = pd.read_csv(filename, sep=" ", header=None, skiprows=1)
 df.columns = ["f1", "f2"]
@@ -11,10 +11,10 @@ pop_size = 32
 # Số thế hệ cụ thể mà bạn muốn biểu diễn
 selected_generations = [1, 5, 10, 50, 100, 1000]
 
-colors = ["b", "g", "r", "c", "m", "y", "k"]
-markers = ["o", "s", "D", "^", "v", "<", ">"]
+colors = ["y", "c", "m", "g", "b", "r", "k"]
+markers = ["<", "s", "^", "D", "v", "o", ">"]
 
-plt.figure(figsize=(12, 8))
+plt.figure(figsize=(12, 9))
 
 for i, gen in enumerate(selected_generations):
     if gen * pop_size <= len(df):
@@ -25,6 +25,7 @@ for i, gen in enumerate(selected_generations):
             label=f"Gen {gen}",
             color=colors[i % len(colors)],
             marker=markers[i % len(markers)],
+            s=70,
         )
         if gen == selected_generations[-1]:
             plt.plot(
@@ -32,13 +33,14 @@ for i, gen in enumerate(selected_generations):
                 gen_data["f2"],
                 "--",
                 color=colors[i % len(colors)],
-                label=f"Gen {gen} (connected)",
+                linewidth=2.5,
+                # label=f"Gen {gen} (connected)",
             )
 
 
 plt.xlabel("number of active sensor")
 plt.ylabel("total energy consumption")
-plt.legend()
+plt.legend(fontsize=19)
 plt.grid(True)
 plt.tight_layout()
 plt.show()
