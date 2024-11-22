42669b78711dbae17086946a89a6bb84b9df53eb
DucChanHUST <kuntakufcvn@gmail.com>
2024-11-22 17:03:34 +0700

    file .sh

---
+++ create_result_folder.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p "./result/f/moead"
+mkdir -p "./result/pareto/moead"
+mkdir -p "./result/r/moead"
+mkdir -p "./result/f/nsga"
+mkdir -p "./result/pareto/nsga"
+mkdir -p "./result/r/nsga"
